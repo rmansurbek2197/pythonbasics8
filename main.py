74964d068413aab3288a24db77d806cc42992869
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-04-18 23:25:49 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-def eng_kichik(toplam):
-    return min(toplam)
+def find_prime_numbers():
+    prime_numbers = []
+    for num in range(1, 101):
+        if num > 1:
+            is_prime = True
+            for i in range(2, num):
+                if num % i == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                prime_numbers.append(num)
+    return prime_numbers
 
-toplam = {12, 45, 7, 23, 56, 89, 34}
-print(eng_kichik(toplam))
+def print_prime_numbers(prime_numbers):
+    for num in prime_numbers:
+        print(num)
+
+def main():
+    prime_numbers = find_prime_numbers()
+    print("1 dan 100 gacha tub sonlar:")
+    print_prime_numbers(prime_numbers)
+
+main()
